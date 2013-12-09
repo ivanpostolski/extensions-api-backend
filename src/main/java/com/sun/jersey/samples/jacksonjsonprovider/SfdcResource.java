5718f0de480559463b730a306a868d95c8d6bd48
Ivan Postolski <ivan.postolski@mulesource.com>
2013-12-09 17:59:18 -0300

    adding get for configurations

--- src/main/java/com/sun/jersey/samples/jacksonjsonprovider/SfdcResource.java
+++ src/main/java/com/sun/jersey/samples/jacksonjsonprovider/SfdcResource.java
@@ -6,7 +6,9 @@ import org.mule.modules.salesforce.extension.SfdcMuleExtension;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 
 @Path("/sfdc")
@@ -14,13 +16,19 @@ public class SfdcResource {
 
     MuleExtension extension = new SfdcMuleExtension();
 
+    @GET
+    @Produces(value = "application/json")
+    public MuleExtensionResource getConfigs(@Context UriInfo ui){
+           return new MuleExtensionResource(extension);
+    }
+
     @Path("/config")
     @Produces(MediaType.APPLICATION_JSON)
     public ConfigurationResource config(){
         return new ConfigurationResource(extension.configurations().get(0));
     }
 
-    @Path("/oauth")
+    @Path("/config-with-oauth")
     @Produces(MediaType.APPLICATION_JSON)
     public ConfigurationResource oauth(){
         return new ConfigurationResource(extension.configurations().get(1));
